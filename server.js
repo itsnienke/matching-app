c4d5ab65cb6e74a26eb846c06cf9546fc597a460
nienke knol <nienkeknol100@gmail.com>
2023-02-20 15:37:27 +0100

    added wiki

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const app = express()
+
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(express.static('static'))
+
+app.get('/hallo', (req, res) => {
+  res.send('<img src="/image/images.jpeg" width="150px">Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app vibing on port ${port}`)
+})
+
+app.get('/home/:user', (req, res) => {
+    console.log(`input van ${req.params.user}`)
+    res.send(`<img src="/image/images.jpeg"><h1>Hello `+ req.params.user + `</h1>`)
 })
